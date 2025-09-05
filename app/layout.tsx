2fab9547f8f115b7ae996405c972879c0d9b3b5a
Chavindu Nuwanpriya <chavindun@gmail.com>
2025-09-05 22:55:40 +0530

    Update layout.tsx
    remove TinyMCE Completely

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
-import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,12 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <Script 
-          src="https://unpkg.com/tinymce@6.8.3/tinymce.min.js" 
-          strategy="beforeInteractive"
-        />
-      </head>
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider 
           attribute="class" 
